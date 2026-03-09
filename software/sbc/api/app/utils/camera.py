ca335279254fda0e5d1610da2c62a0012e4bd6e7
Jakub Klimkowski <sadgi.enjoyer@protonmail.com>
2026-03-09 14:50:44 +0100

    fix: disable reload

--- software/sbc/api/app/utils/camera.py
+++ software/sbc/api/app/utils/camera.py
@@ -2,9 +2,9 @@ import cv2
 
 PIPELINE = (
     "nvarguscamerasrc ! "
-    "video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! "
+    "video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=30/1 ! "
     "nvvidconv flip-method=2 ! "
-    "video/x-raw, width=1280, height=720, format=BGRx ! "
+    "video/x-raw, width=1920, height=1080, format=BGRx ! "
     "videoconvert ! "
     "video/x-raw, format=BGR ! "
     "appsink"
--- software/sbc/api/main.py
+++ software/sbc/api/main.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.app:app",
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", 8000)),
-        reload=True,
+        port=int(os.getenv("PORT", 8000)), reload=False
     )
